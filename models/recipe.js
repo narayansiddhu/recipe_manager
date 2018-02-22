68e857646c6c794c2cecf7ab9e02d2e8512aaef9
narayansiddhu <narayansiddhu@gmail.com>
2018-02-22 08:54:16 +0530

    again from scratch
    Co-authored-by: vigneshwar <vigneshwar.balakrishnan@gmail.com>

---
+++ models/recipe.js
@@ -0,0 +1,39 @@
+let mongoose = require('mongoose');
+
+// Article Schema
+let recipeSchema = mongoose.Schema({
+  title:{
+    type: String,
+    required: true
+  },
+  description:{
+    type: String,
+    required: true
+  },
+  steps:{
+    type: String,
+    required: true
+  },
+  serving:{
+    type: Number,
+    required: true
+  },
+  preptime:{
+    type: Number,
+    required: true
+  },
+  author:{
+    type: String,
+    required: true
+  },
+  pic:{
+    type: String,
+    required: true
+  },
+  body:{
+    type: String,
+    required: true
+  }
+});
+
+let Recipe = module.exports = mongoose.model('Recipe', recipeSchema);
