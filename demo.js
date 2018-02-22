961175df9b99fb52f877520d7317ef3b2509797f
narayansiddhu <narayansiddhu@gmail.com>
2018-02-22 17:29:33 +0530

    alasss
    Co-authored-by: vigneshwar <vigneshwar.balakrishnan@gmail.com>

--- demo.js
+++ demo.js
@@ -7,58 +7,45 @@ mongoose.connect('mongodb://localhost/passport_locals');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
 
-var users;
+// var users;
 
-Account.find().limit(-1).skip(Math.random() * Account.count()).exec(function (err, data) {
-    users = data
-    console.log(users);
-})
-
-// console.log("Printing out users: " + JSON.stringify(users));
-
-// Account.find().exec(function (err, data) {
-//     user = data
-//     console.log(data);
+// Account.find().limit(-1).skip(Math.random() * Account.count()).exec(function (err, data) {
+//     users = data
+//     // console.log(users);
 // })
+
+
 
 async function createRecipe() {
 
 
-    var value = await Account.find().limit(-1).skip(Math.random() * Account.count());
-    console.log(value);
+    // var value = await Account.find().limit(-1).skip(Math.random() * Account.count());
+    // console.log(value);
 
-    // Account.find({},function(err,account){
-    //     if(err){
-    //         console.log('this is failed');
-
-    //     }else{
-    //     console.log("resolving promise")
-    //         users.push(account)
-    //         console.log("pushed to array")
-    //         return account
-    //         console.log("returned array")
-    //         }})
+    var value = await Account.aggregate(
+        { $sample: { size: 1 } }
+     )
         
-// console.log(users)
+  console.log(value[0].username)
+//  console.log(value.length)
+    for (var i = 0; i<value.length; i++ ) {
+        for (var j = 0; j< 3; j++) {
+            var title = faker.name.findName()
+            var description = faker.company.catchPhraseDescriptor()
+            var method = faker.name.jobDescriptor()
+            var serving = faker.random.number({ min: 2, max: 5 })
+            var preptime = faker.random.number({ min: 20, max: 120 })
+            var author = value[i].username
+            var ingredients = faker.commerce.productMaterial()
 
-    // for (var i = 0; i<users.length; i++ ) {
-    //     for (var j = 0; j< 6; j++) {
-    //         var title = faker.name.findName()
-    //         var description = faker.company.catchPhraseDescriptor()
-    //         var method = faker.name.jobDescriptor()
-    //         var serving = faker.random.number({ min: 2, max: 5 })
-    //         var preptime = faker.random.number({ min: 20, max: 120 })
-    //         var author = users[i].username
-    //         var ingredients = faker.commerce.productMaterial()
+            var recipe = new Recipe({ title: title, description: description, method: method, serving: serving, preptime: preptime, author: author, ingredients: ingredients })
+            recipe.save()
+            console.log(recipe)
 
-    //         var recipe = new Recipe({ title: title, description: description, method: method, serving: serving, preptime: preptime, author: author, ingredients: ingredients })
-    //         recipe.save()
-    //         console.log(recipe)
-
-    //     }
-    // }
+        }
+    }
 
 };
 
-// createRecipe();
+createRecipe();
 
