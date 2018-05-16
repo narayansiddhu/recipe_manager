f9625800aca6d7917b4f3f7b2fa818490c9634af
narayansiddhu <narayansiddhu@gmail.com>
2018-05-16 14:10:15 +0530

    update operation working

--- routes/recipe.js
+++ routes/recipe.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 var Recipe = require('../models/recipe');
 var User = require('../models/user');
-router.get('/', ensureAuthenticated, (req, res) => {
+router.get('/',(req, res) => {
     Recipe.find({}, (err, recipes) => {
         if (err) return err;
         else
@@ -14,14 +14,7 @@ router.get('/', ensureAuthenticated, (req, res) => {
 router.get('/add', ensureAuthenticated, (req, res) => {
     res.render('recipe_add');
 });
-function ensureAuthenticated(req, res, next) {
-    if (req.isAuthenticated()) {
-        return next();
-    } else {
-        //req.flash('error_msg','You are not logged in');
-        res.redirect('/users/login');
-    }
-}
+
 router.post('/add', (req, res) => {
     // var title = req.body.title;
     // var description = req.body.description;
@@ -81,42 +74,45 @@ router.delete('/delete/:id', (req, res) => {
 
 
 });
-router.get('/edit/:id',(req,res)=>{
+router.get('/edit/:id', ensureAuthenticated,(req,res)=>{
     Recipe.findById(req.params.id,function(err,recipe){
+        if(recipe.author!=req.user.username){
+            res.redirect('/recipe')
+      }
         res.render('recipe_edit',{
             recipe:recipe
-        })
-    })
+        });
+    });
 
-})
+});
 router.post('/edit/:id', function(req, res){
  
-    let title = req.body.title;
-	let description = req.body.ingredients;
-    let ingredients = req.body.ingredients;
-    let author = req.user.username;
-
+    let recipe = {};
+    recipe.title = req.body.title;
+    recipe.description = req.body.description;
+    recipe.ingredients = req.body.ingredients;
 
     let query = {_id:req.params.id}
-    let recipe = new Recipe({
-        title:title,
-        description:description,
-        ingredients:ingredients,
-        author:author
-    })
-    Recipe.update(query, recipe, function(err){
+ 
+    Recipe.update(query,recipe, function(err){
       if(err){
         console.log(err);
         return;
       } else {
-          console.log('req.body.title');
         req.flash('success_msg', 'Recipe Updated');
         res.redirect('/recipe');
       }
     });
   });
   
-  
+  function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    } else {
+        //req.flash('error_msg','You are not logged in');
+        res.redirect('/users/login');
+    }
+}
 
 
 
